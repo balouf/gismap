6ebb6e7fd839d7a782f238c91b97f08c2a5b283e
Fabien Mathieu <fabien.mathieu@normalesup.org>
2026-05-05 07:33:28 +0200

    Swap Irif for Lip6 in tests

--- tests/test_lab_examples.py
+++ tests/test_lab_examples.py
@@ -5,9 +5,9 @@ from time import time
 import pytest
 
 from gismap.lab_examples.cotel import AlgoRes2016, AlgoRes2026
-from gismap.lab_examples.irif import IrifMap
 from gismap.lab_examples.lamsade import Lamsade
 from gismap.lab_examples.lincs import LINCS
+from gismap.lab_examples.lip6 import Lip6Full
 from gismap.lab_examples.toulouse import LaasMap
 
 TIME_BUDGET = 10.0
@@ -16,14 +16,14 @@ TIME_BUDGET = 10.0
 @pytest.mark.parametrize(
     "lab_cls, min_authors",
     [
-        (IrifMap, 13),
+        (Lip6Full, 50),
         (Lamsade, 36),
         (LINCS, 57),
         (LaasMap, 37),
         (AlgoRes2026, 36),
         (AlgoRes2016, 31),
     ],
-    ids=["graphes", "Lamsade", "LINCS", "sara", "AlgoRes2026", "AlgoRes2016"],
+    ids=["LIP6", "Lamsade", "LINCS", "sara", "AlgoRes2026", "AlgoRes2016"],
 )
 def test_author_iterator(lab_cls, min_authors):
     lab = lab_cls()
